6e9790421b3d9280ca48e1835c97db656efa4db0
felipealmendra <111028663+felipealmendra@users.noreply.github.com>
2023-12-25 00:39:08 -0300

    Update settings.py
    ss

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vb6zg=m#i-cgf#cg$6r-pl==844f6xp*ck9pl9zt6ko!_v$_nn'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
